f346e419c358ff6cb86b2e4582baaf37e6153e3a
subrayahegde <68839512+subrayahegde@users.noreply.github.com>
2025-04-14 14:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import tempfile
 
 # Set your LlamaParse API key (alternatively use st.secrets)
-os.environ["LLAMA_CLOUD_API_KEY"] = "llx-wLCQKwtR0xyjaCenecrhMacRN2SNgaMhuykGqPxYImNb1OSc"
+os.environ["LLAMA_CLOUD_API_KEY"] = "<LLAMACLOUD API KEY>"
 
 # Set page config
 st.set_page_config(page_title="LlamaParse File Extractor", layout="centered")
